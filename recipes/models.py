58ecaaff9abbee14d50ea516b1de83dd8ceea04b
Kauah <menezeskauah@gmail.com>
2024-05-15 11:37:09 -0300

    criando os models

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -15,6 +18,9 @@ class Recipe(models.Model):
     preparation_steps_is_html = models.BooleanField(default=False)
     Dat = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(dafault=False)
+    is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
